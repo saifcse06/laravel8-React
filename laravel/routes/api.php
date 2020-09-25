45cec7dda0cb89622e3ff9c653ba44c4de738760
Md Saif Uddin <saif.uddin@sslwireless.com>
2020-09-25 11:23:19 +0600

    initial commit

---
+++ laravel/routes/api.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Http\Controllers\ExpenseController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+Route::get('/expenses', [ExpenseController::class,'index'])->name('expenses.all');
+Route::post('/expenses', [ExpenseController::class,'store'])->name('expenses.store');
+Route::get('/expenses/{expense}', [ExpenseController::class,'show'])->name('expenses.show');
+Route::put('/expenses/{expense}', [ExpenseController::class,'update'])->name('expenses.update');
+Route::delete('/expenses/{expense}', [ExpenseController::class,'destory'])->name('expenses.destroy');
